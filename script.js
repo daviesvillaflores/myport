7cd58e56e46d97dfffb384e1e41bb757d8d5c44e
daviesvillaflores <79559491+daviesvillaflores@users.noreply.github.com>
2022-12-02 15:22:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // typing text animation script
+     var typed = new Typed(".typing", {
+        strings: ["BSIT STUDENT"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    });
